63de4f01c149d5bb97e38efefb342e99ed6b8d9d
wgtsantos <wgtsantos@gmail.com>
2020-10-06 18:41:17 -0300

    Alterações da Aula de Hoje

--- cadastro.php
+++ cadastro.php
@@ -2,8 +2,8 @@
 <html>
 <head>
 	<title> TOP 5 ANY</title>
-	<meta charset="utf-8">
-	<link rel="stylesheet" type="text/css" href="css/estilo.css" media="screen" />
+	<meta charset="utf-8" />
+	<link rel="stylesheet" type="text/css" href="css/estilo.css" media="screen">
 </head>
 <body>
 
